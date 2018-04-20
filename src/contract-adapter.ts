29dcb38b50945a419a6264008c34a283157b2293
Kirill Maksimov <kirill@orbs.com>
2018-04-21 08:56:40 +0900

    Call contract method

--- src/contract-adapter.ts
+++ src/contract-adapter.ts
@@ -1,21 +1,37 @@
 import * as fs from "fs";
 import { RsaPublicKey } from "crypto";
+const Web3 = require("web3");
+import { getPublicKey } from "./utils";
 
 export interface ContractAdapterInterface {
     queryRequest(query: Buffer, buyerPublicKey: RsaPublicKey): void;
     queryResponse(requestId: string, encryptedQueryResults: Buffer): void;
     getSellerPublicKey(): RsaPublicKey;
+    test(): Promise<void>;
 }
 export default class ContractAdapter implements ContractAdapterInterface {
     address: string;
+    web3: any;
+    contract: any;
 
     constructor(input: { address: string }) {
         this.address = input.address;
+        this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        // FIXME: use real account
+        // this.web3.eth.defaultAccount = this.web3.eth.accounts[0];
+
+        const jsonInterface = JSON.parse(fs.readFileSync(`${__dirname}/../build/contracts/GibuvAroch.json`).toString()).abi;
+        this.contract = new this.web3.eth.Contract(jsonInterface, this.address);
+    }
+
+    async test() {
+        console.log(this.contract);
+        console.log(await this.contract.methods.publicKey().call());
     }
 
     // TODO: replace with a real call
     getSellerPublicKey(): RsaPublicKey {
-        throw new Error(`Not implemented`);
+        return getPublicKey(`${__dirname}/../../temp-keys/seller/key.pub.pem`);
     }
 
     queryRequest(encryptedQuery: Buffer, buyerPublicKey: RsaPublicKey) {
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world");
+import Client from "./client";
+import Server from "./server";
+import ContractAdapter from "./contract-adapter";
+
+const MODE = process.argv[2];
+const { CONTRACT_ADDRESS } = process.env;
+
+if (MODE == "--client") {
+    const client = new Client({ adapter: new ContractAdapter({ address: CONTRACT_ADDRESS }) });
+
+    (async () => {
+        await client.adapter.test();
+    })().then(console.log, console.error);
+
+}
