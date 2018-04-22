bd8f261b63c366412da055b6ba0942c5c4af9982
Doody Parizada <doody.parizada@gmail.com>
2018-04-22 11:06:23 +0900

    update readme

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # gibuvaroch
 
+A hackathon project by Doody Parizada, Nitzan Tomer and Kirill Maximov.
+Created during the LongHash Hackathon Tokyo 2018.
+
+## Demo
+
+https://docs.google.com/presentation/d/13R6VKEuX3yrq7KyjYN4d4E5-8inwyslEGAIjHh7fg78/edit?usp=sharing
+
+## installation guide
 ```bash
-yarn install
+npm run install
 ./generate-keys.sh
-yarn test
+npm run test
+npm run start-node
+npm run contract-test
 ```
-
-```bash
-yarn start-node
-yarn contract-test
-```
-
-FIXME: add functional search, encrypt documents
-FIXME: don't process events twice
--- contracts/GibuvAroch.sol
+++ contracts/GibuvAroch.sol
@@ -3,20 +3,20 @@ pragma solidity ^0.4.23;
 import "./Ownable.sol";
 
 contract GibuvAroch is Ownable {
-	struct PriceList {
-		uint[] prices;
-		bool exists;
-	}
+    struct PriceList {
+    	uint[] prices;
+    	bool exists;
+    }
 
-	mapping (bytes32 => PriceList) private prices;
-	string public publicKey;
+    mapping (bytes32 => PriceList) private prices;
+    string public publicKey;
 
-	event LogQueryRequest(bytes32 reqId, string buyerPublicKey, string encryptedQuery);
-	event LogQueryResponse(bytes32 reqId, uint[] dataPrices, string encryptedQueryResults);
-	event LogDataRequest(bytes32 reqId, uint index);
-	event LogDataResponse(bytes32 reqId, string encryptedData);
+    event LogQueryRequest(bytes32 reqId, string buyerPublicKey, string encryptedQuery);
+    event LogQueryResponse(bytes32 reqId, uint[] dataPrices, string encryptedQueryResults);
+    event LogDataRequest(bytes32 reqId, uint index);
+    event LogDataResponse(bytes32 reqId, string encryptedData);
 
-	constructor(string _publicKey) public {
+    constructor(string _publicKey) public {
         publicKey = _publicKey;
     }
 
@@ -26,38 +26,38 @@ contract GibuvAroch is Ownable {
 
     // queryRequest - called by buyer
     // the query should be encrypted with the publicKey defined by the contract
-	function queryRequest(bytes32 reqId, string buyerPublicKey, string encryptedQuery) public {
-		require(!prices[reqId].exists);
+    function queryRequest(bytes32 reqId, string buyerPublicKey, string encryptedQuery) public {
+    	require(!prices[reqId].exists);
 
-		emit LogQueryRequest(reqId, buyerPublicKey, encryptedQuery);
-	}
+    	emit LogQueryRequest(reqId, buyerPublicKey, encryptedQuery);
+    }
 
     // queryResponse - called by the seller
     // dataPrices contains the prices for the results list encrypted with the
     // buyerPublicKey
-	function queryResponse(bytes32 reqId, uint[] dataPrices, string encryptedQueryResults) public onlyOwner {
-		prices[reqId] = PriceList({
-			prices: dataPrices,
-			exists: true
-		});
+    function queryResponse(bytes32 reqId, uint[] dataPrices, string encryptedQueryResults) public onlyOwner {
+    	prices[reqId] = PriceList({
+    		prices: dataPrices,
+    		exists: true
+    	});
 
-		emit LogQueryResponse(reqId, dataPrices, encryptedQueryResults);
-	}
+    	emit LogQueryResponse(reqId, dataPrices, encryptedQueryResults);
+    }
 
     // dataRequest - called by the buyer
     // the buyer wants to buy the query result with given index.
     // The buyer should pay the ether amount defined by the price in the given index
-	function dataRequest(bytes32 reqId, uint index) public payable {
-		require(prices[reqId].exists);
-		require(index < prices[reqId].prices.length);
-		require(msg.value == prices[reqId].prices[index]);
+    function dataRequest(bytes32 reqId, uint index) public payable {
+    	require(prices[reqId].exists);
+    	require(index < prices[reqId].prices.length);
+    	require(msg.value == prices[reqId].prices[index]);
 
-		emit LogDataRequest(reqId, index);
-	}
+    	emit LogDataRequest(reqId, index);
+    }
 
     // dataResponse - called by the seller
     // returns the requested data encrypted with the buyerPublicKey 
-	function dataResponse(bytes32 reqId, string encryptedData) public onlyOwner {
-		emit LogDataResponse(reqId, encryptedData);
-	}
+    function dataResponse(bytes32 reqId, string encryptedData) public onlyOwner {
+    	emit LogDataResponse(reqId, encryptedData);
+    }
 }
